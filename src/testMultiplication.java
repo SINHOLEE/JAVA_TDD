d94ce45716e3d3d7c05380264fec1a2cd4ab4bd2
이신호 <dltlsgh5@naver.com>
2020-02-24 23:17:05 +0900

    day1

--- src/testMultiplication.java
+++ src/testMultiplication.java
@@ -27,5 +27,15 @@ class testMultiplication {
 		product = five.times(3);
 		assertEquals(15, product.amount);
 	}
+	@Test
+//	10. 우리는 five라는 인스턴스는 Dollar five1 = new Dollar(5);와 같다고 판단하고 싶다. 하지만 현재 그러지 못한다.
+//	11. 그래서 새로운 인스턴스를 만들 때, 이미 같은 value를 지니고 있는 인스턴스가 있다면, 같은 레퍼런스를 key값으로 가지고 있는 hash table에 매칭한다면?
+	public void testEquality() {
+		Dollar a = new Dollar(5);
+		Dollar b = new Dollar(5);
+		System.out.println(a);
+		System.out.println(b);
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+	}
 
 }
